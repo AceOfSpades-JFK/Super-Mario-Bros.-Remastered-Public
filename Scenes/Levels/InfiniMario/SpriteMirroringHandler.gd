361bf008fc41d0d3ddd4092d648b5212a6b9357e
Miguel <kingofspadeslm567@gmail.com>
2025-10-28 00:46:22 -0700

    mirroring in directions depending on where a sprite is in the world

--- Scenes/Levels/InfiniMario/SpriteMirroringHandler.gd
+++ Scenes/Levels/InfiniMario/SpriteMirroringHandler.gd
@@ -1,6 +1,10 @@
 extends Node
 class_name SpriteMirroringHandler
 
+const BORDER_ORIGIN = Vector2(0,0)
+const BORDER_SIZE = Vector2(128, 128)
+const BORDER_RECT = Rect2(BORDER_ORIGIN - BORDER_SIZE / 2, BORDER_SIZE)
+
 @export var node_groups: Array[StringName] = []
 
 var _sprite_collection: Array[Node2D] = []
@@ -20,10 +24,25 @@ func _ready() -> void:
 			nodes.append_array(n.get_children())
 			n = nodes.pop_back()
 		
-		for spr: Node2D in _sprite_collection:
-			# Create the mirror
-			var top_cv = spr.get_canvas_item()
-			add_repeats.call(top_cv, 128)
+		# for spr: Node2D in _sprite_collection:
+		# 	# Create the mirror
+		# 	var top_cv = spr.get_canvas_item()
+		# 	add_repeats.call(top_cv, 128)
+
+func _process(delta: float) -> void:
+	# Setup viewport rect
+	# var viewport_pos: Vector2 = get_viewport().get_camera_2d().position
+	# var viewport_size: Vector2 = get_viewport().get_visible_rect().size
+	# if get_viewport().get_camera_2d().anchor_mode == Camera2D.AnchorMode.ANCHOR_MODE_DRAG_CENTER:
+	# 	viewport_pos = get_viewport().get_camera_2d().position - (viewport_size / 2.0)
+	# var viewport: Rect2 = Rect2(viewport_pos, viewport_size)
+	
+	for spr: Node2D in _sprite_collection:
+		var cv_item: RID = spr.get_canvas_item()
+		if spr.global_position.x < BORDER_ORIGIN.x:
+			RenderingServer.canvas_set_item_repeat(cv_item, Vector2(BORDER_SIZE.x, 0), 1)
+		else:
+			RenderingServer.canvas_set_item_repeat(cv_item, Vector2(-BORDER_SIZE.x, 0), 1)
 
 
 func _on_scene_tree_node_added(n: Node) -> void:
